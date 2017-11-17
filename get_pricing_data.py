46f4bab69036d4484c758b0d0c6ff44f87acf9d0
captainjackcity <32316343+captainjackcity@users.noreply.github.com>
2017-11-17 16:25:59 -0500

    Update get_pricing_data.py

--- get_pricing_data.py
+++ get_pricing_data.py
@@ -2,8 +2,8 @@ import quandl
 import numpy as np
 
 #authtoken = <your_authtoken>
-quandl.ApiConfig.api_key = "9zRuvf743WqaS-MXYfHN"
-#authtoken = quandl.ApiConfig.api_key = "9zRuvf743WqaS-MXYfHN"
+quandl.ApiConfig.api_key = "your_authtoken"
+
 
 #start_date = "pick_start_date"
 start_date = "2016-11-16"
@@ -11,7 +11,7 @@ start_date = "2016-11-16"
 #market = "Pick_market_of_interest"
 market = "BCHARTS/BITSTAMPUSD"
 
-#df = quandl.get("BCHARTS/BITSTAMPUSD", authtoken="9zRuvf743WqaS-MXYfHN", start_date="2016-11-16", parse_dates=True, index_col=0)
+#df = quandl.get("BCHARTS/BITSTAMPUSD", authtoken="your_authtoken", start_date="2016-11-16", parse_dates=True, index_col=0)
 #print(df.head())
 
 df = quandl.get(market, authtoken = quandl.ApiConfig.api_key, start_date= start_date, parse_dates=True, index_col=0)
